46505744fb938025bf5a2d7662d061aa70328d6f
Rishabh Shukla <43636003+RishabhShukla2000@users.noreply.github.com>
2022-10-10 12:53:27 +0530

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿
+using DBF.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AdventureWorks2014Context _context;
+        public ProductController(AdventureWorks2014Context context)
+        { 
+            _context=context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return Ok(await _context.Products.ToListAsync());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+using DBF.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AdventureWorks2014Context>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
